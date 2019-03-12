e03604f7efb14898570f192436a175069b6022d1
Carter Brehm <carter@brehmit.com>
2019-03-12 20:03:49 +0000

    done with assignment 1

---
+++ assignment01/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#first-name').text('Carter');
+    $('#last-name').text('Brehm');
+    $('.mcc').css('color', 'blue');
+    $('.mcc').css('font-style', 'italic');
+    $('ul').html('<li id="hobby">Epic Gaming</li><li id="hobby">Flying</li><li id="hobby">Drilling really really low into the ground</li>');
+});
