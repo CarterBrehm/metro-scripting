cebc4cc4b6d87324b77c6f43aa6d6b35bf96f9e3
CarterBrehm <carter@brehmit.com>
2019-04-15 13:18:55 -0500

    Completed Assignment 6

---
+++ assignment06/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Assignment 6</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <!-- Carter Brehm
+        index.php
+		INFO2340.PLA
+		Jones
+        April 9nd, 2019 -->
+        <h1>Hello, World!</h1>
+        <div>Allow me to introduce myself! My name is <?php print("Carter Brehm")?>!</div>
+        <div>I am <?php print("16")?> years old and I am a student at Metropolitan Community College.</div>
+        <div>I am currently studying <?php print("Computer Programming")?> and I hope to graduate by <?php print("2021")?>.</div>
+</body>
+
+</html>
