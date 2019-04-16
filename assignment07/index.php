f3033af72a80afb4de55dc97880b9d63cfcf2033
Carter Brehm <carter@brehmit.com>
2019-04-16 14:18:03 -0500

    Completed Assignment 7

---
+++ assignment07/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Assignment 7</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <!-- Carter Brehm
+        index.php
+		INFO2340.PLA
+		Jones
+        April 16th, 2019 -->
+        <?php
+            $dinner = "burgers";
+            $stars = 2;
+            $place = "store";
+            $animal = "rock hyrax"
+        ?>
+
+        <h1>Good morning, Nebraska!</h1>
+        <div>Last night I ate <?php print($dinner)?>.</div>
+        <div>While it was good, it wasn't perfect. I'd give it <?php print($stars)?> stars out of 4.</div>
+        <div>Tomorrow I plan to go to the <?php print($place)?>.</div>
+        <div>My favorite animal is the <?php print($animal)?>.</div>
+</body>
+
+</html>
