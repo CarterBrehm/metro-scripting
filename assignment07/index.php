778801cbbd3471f9aa83421b054992e9ba4a643d
Carter Brehm <carter@brehmit.com>
2019-04-23 21:32:34 +0000

    Fixed apache.conf and Assignment 7

--- assignment07/index.php
+++ assignment07/index.php
@@ -4,9 +4,6 @@
 <head>
     <title>Assignment 7</title>
     <meta charset="utf-8">
-    <?php
-        include "includes/header.php";
-    ?>
 </head>
 
 <body>
@@ -14,8 +11,19 @@
         index.php
 		INFO2340.PLA
 		Jones
-        April 23rd, 2019 -->
+        April 16th, 2019 -->
+        <?php
+            $dinner = "burgers";
+            $stars = 2;
+            $place = "store";
+            $animal = "rock hyrax"
+        ?>
 
+        <h1>Good morning, Nebraska!</h1>
+        <div>Last night I ate <?php print($dinner)?>.</div>
+        <div>While it was good, it wasn't perfect. I'd give it <?php print($stars)?> stars out of 4.</div>
+        <div>Tomorrow I plan to go to the <?php print($place)?>.</div>
+        <div>My favorite animal is the <?php print($animal)?>.</div>
 </body>
 
 </html>
