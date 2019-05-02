4b490172983caffef5cbfa7ad3d5bc03b81e3a9f
Carter Brehm <carter@brehmit.com>
2019-05-02 18:52:28 +0000

    Finished Assignment 9

--- assignment09/part02/index.php
+++ assignment09/part02/index.php
@@ -3,21 +3,26 @@
     <title>Assignment 9, Part 2</title>
 </head>
 <body>
-    
+
     <?php
         //Task 3.1: Change these items to reflect
         //four of your favorite things
         $my_favorite_things = array (
-            "My first favorite thing",
-            "My second favorite thing",
-            "My third favorite thing",
-            "My fourth favorite thing"
+            "Epic gaming",
+            "Flying",
+            "Fighting for the inclusion of male deer",
+            "Drilling really low into the ground"
         );
     ?>
 
     <h1>My Favorite Things</h1>
         <!-- Task 3.2: Code to output things below -->
-
-
+        <?php
+            echo("<ul>");
+            for($i = 0; $i < sizeof($my_favorite_things); $i++) {
+                echo("<li>" . $my_favorite_things[$i] . "</li>");
+            }
+            echo("</ul>")
+        ?>
     </body>
 
